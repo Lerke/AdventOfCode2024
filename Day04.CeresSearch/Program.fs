53abcf2c434062e43103242803feeeeb08062f47
Willem <willemsporen@gmail.com>
2024-12-04 08:37:03 +0100

    Day 4 - 1 star

---
+++ Day04.CeresSearch/Program.fs
@@ -0,0 +1,54 @@
+﻿open System
+open System.IO
+
+type XmasBoard(Board) =
+    member val private Board: char array array = Board
+
+    member this.Index(x, y) =
+        match ((y < this.Board.Length && y >= 0), (x < this.Board[0].Length && x >= 0)) with
+        | true, true -> Some(this.Board[y][x])
+        | _ -> None
+
+    member this.Diagonals(x, y) =
+        let sequences =
+            seq {
+                (fun x y i -> (x + i, y + 0))
+                (fun x y i  -> (x + 0, y + i))
+                (fun x y i -> (x - i, y + 0))
+                (fun x y i -> (x + 0, y - i))
+                (fun x y i -> (x + i, y + i))
+                (fun x y i -> (x + i, y - i))
+                (fun x y i -> (x - i, y + i))
+                (fun x y i -> (x - i, y - i))
+            }
+
+        sequences
+        |> Seq.map (fun fn ->
+            (seq { 0 .. "XMAS".Length - 1 }
+             |> Seq.map (fn x y)
+             |> Seq.map this.Index))
+        |> Seq.filter (fun f -> f |> Seq.forall _.IsSome)
+        |> Seq.map (fun f -> (f |> Seq.map(_.Value)))
+
+    member this.XmasInBoard() =
+        seq { 0 .. this.Board.Length - 1 }
+        |> Seq.collect (fun f -> (seq { 0 .. this.Board[0].Length - 1 }) |> Seq.map(fun p -> (f,p)))
+        |> Seq.collect this.Diagonals
+        |> Seq.filter (fun f -> f |> Seq.rev |> String.Concat = "XMAS")
+
+    new(lines: string array) = XmasBoard(lines |> Array.map _.ToCharArray())
+
+match Environment.GetCommandLineArgs() with
+| [| _; file |] ->
+    match File.Exists(file) with
+    | true ->
+        printfn "--- Day 04: Ceres Search ---"
+        let input = File.ReadAllLines file
+        let board = XmasBoard input
+        let hits = board.XmasInBoard()
+
+        printfn $"⭐\tResult:\t%A{hits |> Seq.length}"
+        0
+    | false -> failwithf "File not found"
+| _ -> failwithf "Usage: ./dotnet run <path-to-puzzle-input>"
+|> ignore
